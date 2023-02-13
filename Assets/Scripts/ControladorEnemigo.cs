06cddb2efad31315d83209c3b2f160ce5627a7d7
Eduardo Gómez Blanco <eduardogomezblanco@gmail.com>
2023-02-13 17:17:12 +0100

    Add seguimiento del enemigo
    Creación del código del enemigo para perseguir al jugador.

--- Assets/Scripts/ControladorEnemigo.cs
+++ Assets/Scripts/ControladorEnemigo.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class ControladorEnemigo : MonoBehaviour
 {
+    public Transform objetivo;
+
+    private float velocidad;
+    private Vector3 direccion;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        velocidad = Random.Range(2f, 5f);
+    }
 
     void Update()
     {
-        
+        direccion = objetivo.position - transform.position;
+        direccion.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + direccion * velocidad * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
